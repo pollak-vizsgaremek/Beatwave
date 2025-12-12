03cb64ed37cfaf7a427c659770e6097d463a7cc7
FaboRobi <fabodudasr@gmail.com>
2025-12-12 13:14:29 +0100

    Input Component + Login Page mostly Done

---
+++ frontend/src/utils/Type.ts
@@ -0,0 +1,12 @@
+interface InputProps {
+  labelTile: string;
+  inputType: string;
+  inputName: string;
+  inputPlaceHolder: string;
+  wrapperClassName?: string;
+  labelClassName?: string;
+  inputClassName?: string;
+  forgotPwd?: boolean;
+}
+
+export type { InputProps };
