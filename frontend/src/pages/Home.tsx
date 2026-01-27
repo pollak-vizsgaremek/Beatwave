26b83af2b74f469277a7baddf1725eda5d17ced4
Csilluslol <106381397+Csilluslol@users.noreply.github.com>
2026-01-27 21:28:25 +0100

    Refactor Card component and enhance navigation UI
    Refactored the Card component to display ranking, album art, and additional info in a new layout. Updated Home page to use the new Card layout for top artists and songs, and improved the recently played section. Enhanced Navigation with a user dropdown menu for settings, notifications, and logout functionality. Renamed 'appId' to 'platform' in the ConnectedApp model. + javitott EK diagram

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,15 +5,21 @@ const Home = () => {
     <div className="flex flex-col gap-20 ">
       <div className="flex flex-col items-center justify-center w-full">
         <h1 className="text-3xl font-bold mb-10">Welcome to Beatwave</h1>
-        <Card />
+        <div className="bg-card w-2/6 rounded-lg p-5 flex justify-between">
+          <div className="text-xl">
+            <h2 className="font-semibold">Recently played on [platform]:</h2>
+            <p>[song name]</p>
+          </div>
+          <div>[album pic here]</div>
+        </div>
       </div>
-      <div className="pl-30">
+      <div className="pl-30 ">
         <h2 className="text-2xl font-semibold mb-5">Your top 10 Artists:</h2>
-        <Card />
+        <div className="grid grid-cols-5 gap-5"><Card /> <Card /><Card /><Card /></div>
       </div>
       <div className="pl-30">
         <h2 className="text-2xl font-semibold mb-5">Your top 10 Songs:</h2>
-        <Card />
+       <div className="grid grid-cols-5 gap-5"><Card /> <Card /><Card /><Card /></div>
       </div>
     </div>
   );
