d910fcf952338743fe654c3198a63f2c95b728e9
Csilluslol <muzsik.csilla@gmail.com>
2026-02-02 12:57:44 +0100

    UserProfile

--- backend/src/routes/appRoutes.ts
+++ backend/src/routes/appRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { createUser, authenticateUser } from "../controllers/authController";
+import { getUserProfile } from "../controllers/userProfile";
 
 const router = Router();
 
 router.post("/register", createUser);
 router.post("/login", authenticateUser);
+router.get("UserProfile", getUserProfile);
 
 export default router;
