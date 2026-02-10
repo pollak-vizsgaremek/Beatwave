8d76e750e632035652d1f86651d45d611c3eebe4
Csilluslol <muzsik.csilla@gmail.com>
2026-02-10 13:18:22 +0100

    Top List Componetne

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,14 +1,21 @@
-const Card = () => {
+type CardProps = {
+  key: number;
+  name: string;
+  image: string;
+  placing: number;
+};
+
+const Card = (CardProps: any) => {
   return (
-    <div className="">
+    <div key={CardProps.key} className='mr-12'>
       <div className="bg-card rounded-lg p-5 flex flex-col items-center">
-        <h1 className="font-bold text-xl">#[placing]</h1>
+        <h1 className="font-bold text-xl">#{CardProps.placing}</h1>
         <div>
           <div className="bg-accent w-24 h-24 mb-4 rounded-full flex items-center justify-center">
             [album pic here]
           </div>
-          <h2 className="font-semibold text-lg">[artist/song name]</h2>
-          <p className="text-sm text-gray-400 mt-2">[additional info]</p>
+          <h2 className="font-semibold text-lg">{CardProps.name}</h2>
+          <p className="text-sm text-gray-400 mt-2">{CardProps.image}</p>
         </div>
       </div>
     </div>
