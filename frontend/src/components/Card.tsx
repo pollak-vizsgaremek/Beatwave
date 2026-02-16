0d652ef6cad919bf64c0b8b72c6666d4d03370d1
Csilluslol <muzsik.csilla@gmail.com>
2026-02-16 12:56:03 +0100

    maign toplist component

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -5,17 +5,19 @@ type CardProps = {
   placing: number;
 };
 
-const Card = (CardProps: any) => {
+const Card = ({ key, name, image, placing }: CardProps) => {
   return (
-    <div key={CardProps.key} className='mr-12'>
-      <div className="bg-card rounded-lg p-5 flex flex-col items-center">
-        <h1 className="font-bold text-xl">#{CardProps.placing}</h1>
-        <div>
+    <div key={key} className="mr-12">
+      <div className={`bg-[url(${image})] rounded-lg p-5 flex flex-col items-center border-2 border-black`}>
+        <h1 className="font-bold text-xl">#{placing}</h1>
+        <div className="flex flex-col">
           <div className="bg-accent w-24 h-24 mb-4 rounded-full flex items-center justify-center">
             [album pic here]
           </div>
-          <h2 className="font-semibold text-lg">{CardProps.name}</h2>
-          <p className="text-sm text-gray-400 mt-2">{CardProps.image}</p>
+          <div>
+            <h2 className="font-semibold text-lg">{name}</h2>
+            <p className="text-sm text-gray-400 mt-2">{image}</p>
+          </div>
         </div>
       </div>
     </div>
