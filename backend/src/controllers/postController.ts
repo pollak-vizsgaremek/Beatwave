8f697fc911228e00b7bebd2ba27161d846003f9e
Csilluslol <muzsik.csilla@gmail.com>
2026-04-13 12:43:19 +0200

    view discussion

--- backend/src/controllers/postController.ts
+++ backend/src/controllers/postController.ts
@@ -7,9 +7,44 @@ export const getPosts = async (
   next: NextFunction
 ) => {
   try {
-    const getAllPosts = await prisma.post.findMany()
+    const getAllPosts = await prisma.post.findMany();
 
-    res.status(200).json(getAllPosts)
+    res.status(200).json(getAllPosts);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getPostById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { id } = req.params;
+
+    const getPost = await prisma.post.findUnique({
+      where: { id },
+      include: {
+        user: true,
+        comments: {
+          include: {
+            user: true,
+            replies: {
+              include: {
+                user: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    if (!getPost) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    res.status(200).json(getPost);
   } catch (error) {
     next(error);
   }
